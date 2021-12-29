83e141fcc5701378b9c080955542cce94f49d696
medhuuuu <59966267+medhuuuu@users.noreply.github.com>
2021-12-29 17:30:06 +0600

    Add files via upload

---
+++ files/search.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Oswald:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/books.css">
+    <title> Books</title>
+</head>
+<body>
+
+
+<?php 
+
+$db = mysqli_connect("localhost", "root", "", "readersden");
+
+if (!$db) {
+    die("connection error".mysqli_connect_error());
+} 
+
+if(isset($_POST['search']))
+{
+    $data = $_POST['search'];
+
+    //echo $data;
+
+    $sql = "SELECT title, author_name, genre, price FROM book WHERE title LIKE '%$data%' "; 
+    $result = mysqli_query($db, $sql);
+    if (mysqli_num_rows($result)>0) {
+        while ($row= mysqli_fetch_assoc($result)) {
+            ?>
+
+            <div class="card books">
+                <div class="card-body">
+                    <h5 class="card-title" style=" color: rgb(189, 160, 107);"> <?php echo $row['title']; ?> </h5>
+                    <p class="card-text"> <?php echo "Author: ".$row['author_name']; ?> </p>
+                    <p class="card-text"> <?php echo "Genre: ".$row['genre']; ?> </p>
+                    <p class="card-text"> <?php echo "Tk-".$row['price']; ?> </p>
+                    <a href="#" class="button">ORDER NOW</a>
+                </div>
+            </div>
+
+<?php
+        }
+    }
+}
+
+if(isset($_POST['search']))
+{
+    $data = $_POST['search'];
+
+    //echo $data;
+
+    $sql = "SELECT title, author_name, genre, price FROM book WHERE author_name LIKE '%$data%' "; 
+    $result = mysqli_query($db, $sql);
+    if (mysqli_num_rows($result)>0) {
+        while ($row= mysqli_fetch_assoc($result)) {
+            ?>
+
+            <div class="card books">
+                <div class="card-body">
+                    <h5 class="card-title" style=" color: rgb(189, 160, 107);"> <?php echo $row['title']; ?> </h5>
+                    <p class="card-text"> <?php echo "Author: ".$row['author_name']; ?> </p>
+                    <p class="card-text"> <?php echo "Genre: ".$row['genre']; ?> </p>
+                    <p class="card-text"> <?php echo "Tk-".$row['price']; ?> </p>
+                    <a href="#" class="button">ORDER NOW</a>
+                </div>
+            </div>
+
+<?php
+        }
+    }
+}
+
+if(isset($_POST['search']))
+{
+    $data = $_POST['search'];
+
+    //echo $data;
+
+    $sql = "SELECT title, author_name, genre, price FROM book WHERE genre LIKE '%$data%' "; 
+    $result = mysqli_query($db, $sql);
+    if (mysqli_num_rows($result)>0) {
+        while ($row= mysqli_fetch_assoc($result)) {
+            ?>
+
+            <div class="card books">
+                <div class="card-body">
+                    <h5 class="card-title" style=" color: rgb(189, 160, 107);"> <?php echo $row['title']; ?> </h5>
+                    <p class="card-text"> <?php echo "Author: ".$row['author_name']; ?> </p>
+                    <p class="card-text"> <?php echo "Genre: ".$row['genre']; ?> </p>
+                    <p class="card-text"> <?php echo "Tk-".$row['price']; ?> </p>
+                    <a href="#" class="button">ORDER NOW</a>
+                </div>
+            </div>
+
+<?php
+        }
+    }
+}
+
+
+?>
+
+
+
+
